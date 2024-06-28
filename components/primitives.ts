291e5386af3538d25f1d99313742482ff1e994bf
ade.rizaldi.mm2 <ade.rizaldi.mm2@gmail.com>
2024-06-28 16:36:38 +0000

    fix: font size

--- components/primitives.ts
+++ components/primitives.ts
@@ -16,7 +16,7 @@ export const title = tv({
     size: {
       sm: "text-3xl lg:text-4xl",
       md: "text-[2.3rem] lg:text-5xl leading-9",
-      lg: "text-4xl lg:text-6xl",
+      lg: "text-3xl md:text-4xl lg:text-6xl",
     },
     fullWidth: {
       true: "w-full block",
